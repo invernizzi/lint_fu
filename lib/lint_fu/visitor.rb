6d0bb1886c92e557ef5057fc387cf785f81fbb40
Tony Spataro <tony@rightscale.com>
2010-01-23 14:41:30 -0800

    Add support for multi-line annotation comments.

--- lib/lint_fu/visitor.rb
+++ lib/lint_fu/visitor.rb
@@ -1,5 +1,6 @@
 module LintFu
   class Visitor < SexpProcessor
+    COMMENT                  = /^\s*#/
     VERBOSE_BLESSING_COMMENT = /#\s*security\s*[-:]\s*not\s*a?n?\s*(.*) because (.*)/i
     BLESSING_COMMENT         = /#\s*security\s*[-:]\s*not\s*a?n?\s*(.*)/i
 
@@ -19,8 +20,13 @@ module LintFu
     def blessed?(sexp, issue_class)
       comments = preceeding_comments(sexp)
       return false unless comments
-      match = VERBOSE_BLESSING_COMMENT.match(comments)
-      match = BLESSING_COMMENT.match(comments) unless match
+
+      match = nil
+      comments.each do |line|
+        match = VERBOSE_BLESSING_COMMENT.match(line)
+        match = BLESSING_COMMENT.match(line) unless match
+        break if match
+      end
 
       return false unless match
       blessed_issue_class = match[1].downcase.split(/\s+/).join('_').camelize
@@ -38,8 +44,23 @@ module LintFu
 
     def preceeding_comments(sexp)
       @file_contents ||= File.readlines(self.file)
-      line = @file_contents[sexp.line - 1 - 1];
-      return line if line =~ /^\s*#/
+
+      comments = ''
+
+      max_line = sexp.line - 1 - 1
+      max_line = 0 if max_line < 0
+      min_line = max_line
+
+      while @file_contents[min_line] =~ COMMENT && min_line >= 0
+        min_line -= 1
+      end
+
+      if @file_contents[max_line] =~ COMMENT
+        min_line +=1 unless min_line == max_line
+        return @file_contents[min_line..max_line]
+      else
+        return nil
+      end
     end
   end
 end
